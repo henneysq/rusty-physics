eda495789e8325146496c5aef40a2de12b76bff9
Mark <mah@optoceutics.com>
2022-12-28 15:32:32 +0100

    Added some comments about rectangles

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+// Define a struct called Rectangle with
+// some geometric properties
 #[derive(Debug)]
 struct Rectangle {
     length: u32,
     width: u32,
 }
 
+// Define some functions (implementations) for the
+// Rectangle object
 impl Rectangle {
     fn new(length: u32, width: u32) -> Rectangle {
         Rectangle {
@@ -28,10 +32,16 @@ impl Rectangle {
 }
 
 fn main() {
+    // Declare a mutable rectangle object
     let mut rect1 = Rectangle::new(10, 5);
+    // Clone it using the rectangle's `clone` function
     let mut rect2 = rect1.clone();
+
+    // Set length or width
     rect1.set_length(20);
     rect2.set_width(20);
+
+    // Print the rectangles
     println!("{:?}", rect1);
     println!("{:?}", rect2);
 }
