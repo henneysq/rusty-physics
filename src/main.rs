0bbf2eb069de0e596e6252f4d86ee0bf8b3e650a
Mark <mah@optoceutics.com>
2022-12-28 19:00:38 +0100

    Removed everything and made an oscillator

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,72 @@
-// Define a struct called Rectangle with
-// some geometric properties
-#[derive(Debug)]
-struct Rectangle {
-    length: u32,
-    width: u32,
+use std::{thread, time, f32::consts::PI};
+
+struct Oscillator {
+    // An oscillator oscillating with a frequency,
+    // amplitude, and phase
+    freq: f32, // [Hz]
+    phase: f32, // [rad]
+    amplitude: f32,
+    sig: (f32, f32),
+
 }
 
-// Define some functions (implementations) for the
-// Rectangle object
-impl Rectangle {
-    fn new(length: u32, width: u32) -> Rectangle {
-        Rectangle {
-            length,
-            width,
-        }
+impl Oscillator {
+    fn new(freq: f32, amplitude: f32, phase: f32) -> Oscillator {
+        // Create new oscillator
+        //
+        // freq: oscillation frequency [Hz]
+        // phase: initial oscillation phase [radians]
+        Oscillator { 
+            freq: freq, 
+            amplitude: amplitude, 
+            phase: phase, 
+            sig: (0.0, 0.0) }
     }
 
-    fn clone(&self) -> Rectangle {
-        Rectangle {
-            ..*self
-        }
+    fn set_state(&mut self, t: f32) {
+        // Sets a new state of the oscillator.
+        // t: time in seconds
+        let osc: f32 = t * self.freq * (2.0 * PI) + self.phase;
+        self.sig.0 = self.amplitude * osc.sin();
+        self.sig.1 = self.amplitude * osc.cos();
     }
+}
 
-    fn set_length(&mut self, width: u32) {
-        self.width = width
-    }
+fn main() {    
+    // Define some parameters for an oscillation
+    let freq: f32 = 0.25; // [Hz]
+    let ampl: f32 = 2.0;
+    let phase: f32 = 0.0;
+    let mut oscillator = Oscillator::new(
+        freq, ampl, phase
+    );
+
+    // Get a time loop going
+    let dt: u64 = 100; // ms
+    let dur = time::Duration::from_millis(dt);
+    let mut t: u64 = 0;
     
-    fn set_width(&mut self, length: u32) {
-        self.length = length
+    loop {
+        // Check time at beginning 
+        let now = time::Instant::now();
+
+        // Update the state of the oscillator
+        let time: f32 = (t * dt) as f32;
+        oscillator.set_state(time / 1000.0);
+
+        let deb_amplitude: f32 = (oscillator.sig.0.powf(2.0) + 
+            oscillator.sig.1.powf(2.0)).sqrt();
+        
+        // println!("{} seconds passed", time / 1000.0);
+        println!("Complex signal: {}, {}, - Amplitude: {}", 
+            oscillator.sig.0, oscillator.sig.1, deb_amplitude);
+        
+        // Compensate for computation time; then sleep
+        thread::sleep(dur - now.elapsed());
+        
+        if t >= 10 {
+            break;
+        }
+        t += 1;
     }
-}
-
-fn main() {
-    // Declare a mutable rectangle object
-    let mut rect1 = Rectangle::new(10, 5);
-    // Clone it using the rectangle's `clone` function
-    let mut rect2 = rect1.clone();
-
-    // Set length or width
-    rect1.set_length(20);
-    rect2.set_width(20);
-
-    // Print the rectangles
-    println!("{:?}", rect1);
-    println!("{:?}", rect2);
 }
